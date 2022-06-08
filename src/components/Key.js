3b13f8e201f327723f7d727ae754ba489591e126
Wiesa Anwari <wiesa@wanwari.ca>
2022-06-08 12:57:20 -0400

    style: adjust key hover effects

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,6 +1,6 @@
 const Key = ({ letter, keyboardPressed, mainLetter }) => {
 
-    const colour = mainLetter ? "bg-yellow-300" : "bg-slate-300";
+    const colour = mainLetter ? "bg-yellow-300 hover:bg-yellow-400" : "bg-slate-300 hover:bg-slate-400 hover:shadow-lg";
 
     return(
         <input 
@@ -8,7 +8,7 @@ const Key = ({ letter, keyboardPressed, mainLetter }) => {
             onClick={() => keyboardPressed(letter)} 
             type="button" 
             value={ letter } 
-            className={colour + "   px-4 py-3 m-1 text-black text-lg leading-tight uppercase rounded shadow-xl cursor-pointer"}
+            className={colour + "   px-5 py-4 m-1.5 text-black text-lg leading-tight uppercase rounded cursor-pointer"}
         />
     );
 }
