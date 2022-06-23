c945d2dba774057cee423e18ade4af75cdc74f36
Wiesa Anwari <wiesa@wanwari.ca>
2022-06-23 14:20:03 -0400

    feat(server): get the letters, key letter, pangram, and answers for a given days puzzle

--- server/index.js
+++ server/index.js
@@ -10,19 +10,78 @@ const app = express();
 const parseText = data => {
 
     const htmlAnswerList = data.substring(data.indexOf("<ul class=\"column-list\">"), data.indexOf("</ul>"))
+   
+    const answers = getAnswers(htmlAnswerList);
+    const panagram = getPangram(htmlAnswerList);
+    const gameLetters = getGameLetters(panagram)
+    const keyLetter = getKeyLetter(data);
+
+    console.log("Letters: " + gameLetters);
+    console.log("Key Letter: " + keyLetter);
+    console.log("Panagram: " + panagram);
+    console.log("Answers: " + answers);
+
+    getKeyLetter(data);
+}
+
+/*
+ * Remove the HTML tags and place the words into an array
+*/
+const getAnswers = htmlAnswerList => {
 
     //https://stackoverflow.com/a/24350326
     const htmlRemovedAnswerList = htmlAnswerList.replace(/<[^>]*>/g, ' ')
-        .replace(/\s{2,}/g, ' ')
-        .trim();
+    .replace(/\s{2,}/g, ' ')
+    .trim();
 
     const answers = htmlRemovedAnswerList.split(" ");
-    console.log(answers);
 
+    return answers;
+}
+
+/*
+ * Substring through the HTML source to find the JS code that contains the canvas for the
+ * 'Center Letter Frequency. Then go through that canvas to find the highlighted letter 
+ * for todays letter.
+*/
+const getKeyLetter = data => {
+
+    const dataSubS = data.substring(0, data.indexOf("\"plotX\":[\"a\"")-2);
+    const colours = dataSubS.substring(dataSubS.length - 261);
+    const coloursArray = colours.replace(/\"/g, "").split(",");
+    const keyLetter = String.fromCharCode(coloursArray.indexOf("firebrick") + 97);
+
+    return keyLetter;
+}
+
+/*
+ * Get the pangram for todays riddle
+ * The panagram is the element surrounded by the <strong> tag
+*/
+const getPangram = htmlAnswerList => {
+
+    const panagram= htmlAnswerList.substring(htmlAnswerList.indexOf("<strong>") + 8, htmlAnswerList.indexOf("</strong"));
+
+    return panagram;
+}
+
+/*
+ * Use the pangram to find the game letters
+ * The pangram contains each letter (could be duplicate) so just take each letter
+*/
+const getGameLetters = pangram => {
+
+    let gameLetters = [];
+    for (let i = 0; i < pangram.length; i++) {
+        if (!gameLetters.includes(pangram[i]))
+            gameLetters.push(pangram[i]);
+    }
+
+    return gameLetters; 
 }
 
 app.get("/", (req, res) => {
-    const url = "https://nytbee.com/";
+    const url = "https://nytbee.com";
     fetch(url)
         .then(response => response.text())
         .then(data => parseText(data));
